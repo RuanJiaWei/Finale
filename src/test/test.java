0af3e49df03b016ed2aab39fa738ea854fd84107
Xue <370923205@qq.com>
2016-11-16 01:04:05 +0800

    Test:Split

--- src/test/test.java
+++ src/test/test.java
@@ -1,15 +1,29 @@
 package test;
-
+import java.io.BufferedReader;
 import connection.conn;
 
-public class test {
-
+public class test 
+{
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{	
+		BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
 		// TODO Auto-generated method stub
-		new conn().getCon();
+		// new conn().getCon();
+		String exp;
+		exp = buff.readLine();
+		SP(exp);
 	}
-
+	 
+	public static void SP(String str)
+	{	
+		String[] stringarray = str.split("\\+");
+		for(String s: stringarray)
+		{	
+			System.out.println(s);
+		}
+		
+	}
 }
