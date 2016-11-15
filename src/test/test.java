3fa4ee9d0628f2f359352af9c00a0d63b0a1ef61
Xue <370923205@qq.com>
2016-11-16 01:13:09 +0800

    Test

--- src/test/test.java
+++ src/test/test.java
@@ -8,35 +8,28 @@ public class test
 	 * @param args
 	 */
 	public static void main(String[] args) 
-	{	
-		BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+	{	BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
 		String exp;
 		exp = buff.readLine();
-		SP(exp);
-	}
+		SP(exp);}
 	 
 	public static void SP(String str)
-	{	
-		String[] stringarray = str.split("\\+");
+	{	String[] stringarray = str.split("\\+");
 		for(String s: stringarray)
-		{	
-			System.out.println(s);
-		}
-		
-	}
+		{	System.out.println(s);}}
 	
-	private static String simplify()
-	{	ArrayList<IntegerString> arraylist = new ArrayList<IntegerString>();
-		Map<String, Integer> map = new HashMap<String, Integer>();
+	private static String simplify(String expression)
+	{	//ArrayList<IntegerString> arraylist = new ArrayList<IntegerString>();
+		//Map<String, Integer> map = new HashMap<String, Integer>();
 		StringBuilder stringbuff = new StringBuilder();
 		String[] stringarray = expression.split("\\+");
 		
 		for (String str : stringarray)
 		{ 	int constant = 1;
 			int number1;
-			List<String> varibles = new ArrayList<String>();
+			//List<String> varibles = new ArrayList<String>();
 			String[] stringarray1 = str.split("\\*");
-			IntegerString integerstring1 = new IntegerString();
+			//IntegerString integerstring1 = new IntegerString();
 			StringBuilder stringbuff1 = new StringBuilder();
 			for (String element : stringarray1)
 			{	try
@@ -44,29 +37,29 @@ public class test
 					number1 = Integer.parseInt(element);
 					constant *= number1;}
 				catch (Exception e)
-				{	varibles.add(element);}}
-			
-			for (String var : varibles)
-			{	stringbuff1.append(var);}
-			
-			integerstring1.stringvarible = stringbuff1.toString();
-			integerstring1.number = constant;
-			arraylist.add(integerstring1);}
+				{	/*varibles.add(element);*/}}
 		
-		for (IntegerString intstr : arraylist)
+			//for (String var : varibles)
+			//{	stringbuff1.append(var);}
+			
+			//integerstring1.stringvarible = stringbuff1.toString();
+			//integerstring1.number = constant;
+			/*arraylist.add(integerstring1);*/}
+		
+		/*for (IntegerString intstr : arraylist)
 		{ 	String key = intstr.stringvarible;
 			int value = intstr.number;
 			if (map.containsKey(key)==false) 
 			{	map.put(key, value);}
 			else if (map.containsKey(key)==true) 
 			{	value += map.get(key);
-				map.put(key, value);}}
+				map.put(key, value);}}*/
 		
-		for (String keyinset : map.keySet())
-		{	stringbuff.append(map.get(keyinset).toString() + keyinset + '+');}
+		//for (String keyinset : map.keySet())
+		//{	stringbuff.append(map.get(keyinset).toString() + keyinset + '+');}
 		
 		int len = stringbuff.length();
 		stringbuff.deleteCharAt(len - 1);
 		System.out.println(stringbuff.toString());
-		return stringbuff.toString();}}
+		return stringbuff.toString();}
 }
